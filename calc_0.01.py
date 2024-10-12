bba61f083dc9b76421b23675d23da7f5ba9c7ef1
Katnax <zach.po@hotmail.com>
2024-10-12 22:20:38 +0200

    variable changes

--- calc_0.01.py
+++ calc_0.01.py
@@ -3,17 +3,18 @@ import sys
 
 
 # pi = math.pi
-# x, y, z = None, None, None
-x = None
-y = None
-z = None
+# first_number, second_number, result = None, None, None
+first_number = None
+second_number = None
+result = None
 
-while isinstance(x, float) != True:
-    x = input()
-    print(type(x))
-    x = float(x)
-    print(type(x))
-
+while isinstance(first_number, float) != True:
+    first_number = input()
+    print(type(first_number))
+    first_number = float(first_number)
+    print(type(first_number))
+    if isinstance(first_number, float) != True:
+        print("input correct number")
 
 print("input operator")
 operator = input()
@@ -21,24 +22,28 @@ if operator not in '+-*/':
     print("please enter operator")
     sys.exit()
 
-while isinstance(y, float) != True:
-    y = input()
-    y = float(y)
+while isinstance(second_number, float) != True:
+    second_number = input()
+    second_number = float(second_number)
+    if isinstance(second_number, float) != True:
+        print("input correct number")
+
+
 
 match operator:
     case r"+":
-        z = x + y
+        result = first_number + second_number
     case "-":
-        z = x - y
+        result = first_number - second_number
     case "*":
-        z = x * y
+        result = first_number * second_number
     case "/":
-        z = x / y
+        result = first_number / second_number
 
-if int(z) == z:
-    print(int(z))
+if int(result) == result:
+    print(int(result))
 else:
-    print(z)
+    print(result)
 
 
 
